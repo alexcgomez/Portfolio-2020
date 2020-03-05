813a90300d66d20c9aa9efa1a0d834dacb175bbd
Alex <alexcaceres93@gmail.com>
2020-03-05 16:34:43 +0100

    Añadida página Projects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,14 @@
+let mainpage = document.querySelector("#div-content");
+let navMenu = document.querySelectorAll("nav li");
+console.log(navMenu);
 
+navMenu[3].addEventListener("click", event => {
+  event.preventDefault();
+  mainpage.style.display = "none";
+});
 
-// let skill_html = document.getElementsByClassName('fab fa-html5');
-
-
-
-// skill_html.onmouseover = function() {
-//     this.style.background = "blue";
-// }
+for (let index = 0; index <= 2; index++) {
+  navMenu[index].addEventListener("click", event => {
+    mainpage.style.display = "inherit";
+  });
+}
