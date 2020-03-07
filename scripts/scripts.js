38537009e1e4276965f51f90687e5306c5db8195
Alex <alexcaceres93@gmail.com>
2020-03-07 11:47:44 +0100

    Página de projectos  terminada, añadiendo nuevos proyectos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,33 @@
-let mainpage = document.querySelector("#div-content");
-let navMenu = document.querySelectorAll("nav li");
-console.log(navMenu);
+//////////////// CHANGE PAGE ////////////////////////////////////
 
-navMenu[3].addEventListener("click", event => {
-  event.preventDefault();
-  mainpage.style.display = "none";
-});
+// let mainpage = document.querySelector("#div-content");
+// let navMenu = document.querySelectorAll("nav li");
+// console.log(navMenu);
 
-for (let index = 0; index <= 2; index++) {
-  navMenu[index].addEventListener("click", event => {
-    mainpage.style.display = "inherit";
-  });
+// navMenu.forEach(button => {
+//   button.addEventListener("click", event => {
+//     mainpage.style.display = "inherit";
+//   });
+// });
+
+//////////////// BUTTON TO TOP ////////////////////////////////////
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
